2e1553f0299f30b470f76a0e7bc2d15c410115ba
Dr. Moo <awesomedigger1021@gmail.com>
2020-07-10 22:19:28 +0000

    no print

--- tests/gradient_decent.py
+++ tests/gradient_decent.py
@@ -14,21 +14,18 @@ def six_variable_function(x1, x2, x3, x4, x5, x6):
 min = GradientDescent(single_variable_function)
 
 gradient = min.compute_gradient(delta=0.01)
-print(gradient)
 
 assert gradient == (-2.0000000000000018, 0.0, 0.0), 'Gradient should be (-2.0000000000000018, 0.0, 0.0) but was actually '+str(gradient)
 
 min = GradientDescent(two_variable_function)
 
 gradient = min.compute_gradient(delta=0.01)
-print(gradient)
 
 assert gradient == (-2.0000000000000018, 3.0001000000000055, 0.0), 'Gradient should be [-2.0000000000000018, 3.0001000000000055, 0.0] but was actually '+str(gradient)
 
 min = GradientDescent(three_variable_function)
 
 gradient = min.compute_gradient(delta=0.01)
-print(gradient)
 
 assert gradient == (-2.0000000000000018, 3.0001000000000055, -4.0004000000000035), 'Gradient should be (-2.0000000000000018, 3.0001000000000055, -4.0004000000000035) but was actually '+str(gradient)
 
