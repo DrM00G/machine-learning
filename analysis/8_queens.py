1f8c295c1d4909028f583d6f4350d89ea3b24d9f
Dr. Moo <awesomedigger1021@gmail.com>
2021-01-19 05:46:26 +0000

    Added and tested the  steepest_descent_optimizer function which uses a random_queen_move helper function.

--- analysis/8_queens.py
+++ analysis/8_queens.py
@@ -15,6 +15,7 @@ def calc_cost(locations):
   for coord_1 in locations:
     for coord_2 in locations:
       if coord_1 != coord_2:
+        # print(str(coord_1)+","+str(coord_2))
         if coord_1[0] == coord_2[0]:
           # print(str(coord_1)+"&"+str(coord_2))
           cost=cost+1
@@ -47,21 +48,46 @@ def random_optimizer(n):
   return final_dict
 
 
-locations = [(0,0), (6,1), (2,2), (5,3), (4,4), (7,5), (1,6), (2,6)]
-show_board(locations)
-print(calc_cost(locations))
-print("Optimizer Tests:")
-for n in [10,50,100,500,1000]:
-  rand_dict=random_optimizer(n)
-  print(rand_dict["cost"])
-  if rand_dict["cost"]==0:
-    show_board(rand_dict['locations'])
-# print("Final Solution")
-# cost=100
-# locs=[]
-# while cost!=0:
-#   rand_dict=random_optimizer(2000)
-#   cost=rand_dict["cost"]
-#   locs=rand_dict["locations"]
-# show_board(locs)
+# locations = [(0,0), (6,1), (2,2), (5,3), (4,4), (7,5), (1,6), (2,6)]
+# show_board(locations)
+# print(calc_cost(locations))
+# print("Optimizer Tests:")
+# for n in [10,50,100,500,1000]:
+#   rand_dict=random_optimizer(n)
+#   print(rand_dict["cost"])
+#   if rand_dict["cost"]==0:
+#     show_board(rand_dict['locations'])
 
+
+def steepest_descent_optimizer(n):
+  original_rand_dict=random_optimizer(1000)
+  best_dict=original_rand_dict
+  for j in range(n):
+    rand_otimp_dict=original_rand_dict
+    for queen in rand_otimp_dict["locations"]:
+      rand_otimp_dict["locations"][rand_otimp_dict["locations"].index(queen)]=random_queen_move(queen,rand_otimp_dict["locations"])
+      # print(rand_otimp_dict["locations"])
+
+    if calc_cost(rand_otimp_dict["locations"])<best_dict['cost']:
+        best_dict = {
+          'locations': rand_otimp_dict["locations"],
+          'cost': calc_cost(rand_otimp_dict["locations"])
+        }
+  return best_dict
+
+
+def random_queen_move(queen,loc_dict):
+    directions=[(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
+    direction=random.choice(directions)
+    new_point= (queen[0]+direction[0],queen[1]+direction[1])
+    if -1 not in new_point and 8 not in new_point and new_point not in loc_dict:
+      # print(new_point)
+      return new_point
+    else:
+      # print("had to")
+      return random_queen_move(queen,loc_dict)
+
+print("steepest_descent_optimizer test:")
+for n in [1,5,10,100,5000]:
+  print(n)
+  print(steepest_descent_optimizer(n)['cost'])
